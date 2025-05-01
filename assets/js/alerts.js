4142b3d4aca24a359a70b2ea9a657652162f06d9
unknown <ruminahuelwalter@gmail.com>
2025-05-01 17:58:51 -0300

    prompt, confirm y alert

---
+++ assets/js/alerts.js
@@ -0,0 +1,10 @@
+
+
+alert('Hola mundo');
+
+let nombre = prompt('¿Whats your name');
+console.log('****' +  nombre + '****' );
+
+const seleccion = confirm('Está seguro de borrar esto?');
+console.log(seleccion);
+console.log( global );
