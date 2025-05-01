735f00813eaa20ceea298c0794801d259aae634d
ruminahuelwalter <ruminahuelwalter@gmail.com>
2025-04-30 23:12:51 -0300

    Introducción a la consola

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 // console.log('hola mundo');
 
 let a = 10, 
-    b= 20;
+    b= 20,
+    c= 'Spiderman';
 
+
+const saludo = a + ' ' + c;
+
+console.log('a', a);
+console.log({a});
+console.log('%c Mis variables','color:green;', b);
+
+console.table({a,b,c})
+
+console.log(saludo)
