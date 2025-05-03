b9af4800b47b48e4a9c8e3b368ed2e42abb58976
unknown <ruminahuelwalter@gmail.com>
2025-05-02 22:52:14 -0300

    intro general a los tipos primitivos

---
+++ assets/js/primitivos.js
@@ -0,0 +1,39 @@
+
+let nombre = 'Peter Parker';
+console.log(nombre);
+
+nombre = 'Ben Parker';
+
+nombre = "Tía May";
+nombre = `Tía May`
+
+console.log( typeof nombre );
+
+nombre = 123;
+
+console.log( typeof nombre );
+
+let esMarvel = true;
+console.log( typeof esMarvel);
+
+
+let edad = 32;
+console.log(typeof edad);
+
+edad = 32.5;
+console.log(typeof edad);
+
+
+let superPoder;
+console.log(typeof superPoder);
+
+let soyNull = null;
+console.log(typeof soyNull);
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log( typeof symbol1 );
+
+console.log(symbol1 ==symbol2);
+ 
