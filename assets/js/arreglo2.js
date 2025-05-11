bbf011029b10522da613b9ce871abf7c3dc78391
unknown <ruminahuelwalter@gmail.com>
2025-05-10 23:50:04 -0300

    método push

--- assets/js/arreglo2.js
+++ assets/js/arreglo2.js
@@ -1,6 +1,18 @@
 let juegos = ['Zelda', 'Mario', 'Metroid'];
 console.log('Largo', juegos.length)
 
+
+let primero = juegos[0];
+let ultimo = juegos[ juegos.length - 1 ];
+
+console.log({ primero, ultimo })
+
+
 juegos.forEach( (elemento, indice, arr) => {
     console.log({ elemento, indice, arr});
 });
+
+let nuevaLongitud = juegos.push( 'F-Zero ');
+
+console.log('Nueva longitud:', nuevaLongitud);
+
