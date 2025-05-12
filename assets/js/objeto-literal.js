e262702a6948314edb60fa822275b8e4d3a6427d
unknown <ruminahuelwalter@gmail.com>
2025-05-12 18:23:02 -0300

    Mas datos para el objeto literal

--- assets/js/objeto-literal.js
+++ assets/js/objeto-literal.js
@@ -1,4 +1,34 @@
 
 let personaje = {
-    nombre: 'Tony Stark'
-}
+    nombre: 'Tony Stark',
+    codeName: 'Ironman',
+    vivo: false,
+    edad: 40,
+    coords: {
+        lat: 34.034,
+        lng: -118.70
+    },
+    trajes: ['Mark I', 'Mark V', 'Hulkbuster'],
+    direccion: {
+        zip: '10990, 90265',
+        ubicacion: 'Malibu, California'
+    },
+    'ultima-pelicula': 'Infinity War'
+};
+
+console.log( {personaje});
+console.log( 'Nombre', personaje.nombre);
+console.log( 'Nombre', personaje['nombre']);
+console.log('Edad', personaje.edad);
+
+console.log('Coors', personaje.coords);
+console.log('Lat', personaje.coords.lat);
+
+console.log('No. trajes: ', personaje.trajes.length);
+console.log('Ultimo trajes: ', personaje.trajes[personaje.trajes.length - 1]);
+
+
+const x = 'vivo';
+console.log('Vivo', personaje[x]);
+
+console.log('Última pelicula:', personaje['ultima-pelicula'])
