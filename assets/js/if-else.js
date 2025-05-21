0fa812b33451417dd78cdcc51a68d849821180f6
ruminahuelwalter <ruminahuelwalter@gmail.com>
2025-05-20 23:50:40 -0300

    If-else

---
+++ assets/js/if-else.js
@@ -0,0 +1,14 @@
+
+let a = 5;
+
+if ( a >= 10 ){
+    console.log('A es mayor o igual a 10')
+}
+else{
+    console.log('A es menor a 10');
+}
+/* if ( a < 10 ){
+    console.log('A es menor a 10')
+} */
+
+console.log('Fin del programa');
