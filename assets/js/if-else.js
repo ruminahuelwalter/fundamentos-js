10e051f707c5252ad21e289ba55eb916e76cd8a3
ruminahuelwalter <ruminahuelwalter@gmail.com>
2025-05-22 19:42:11 -0300

    else

--- assets/js/if-else.js
+++ assets/js/if-else.js
@@ -12,3 +12,19 @@ else{
 } */
 
 console.log('Fin del programa');
+
+const hoy = new Date();
+let dia = hoy.getDay();
+
+console.log(dia);
+
+if (dia === 0){
+    console.log('Domingo');
+} else if (dia === 1 ){
+        console.log('Lunes');
+} else if ( dia ===1 ){
+    console.log('Lunes');
+} else {
+    console.log('No es lunes, martes o domingo...');
+    
+}
